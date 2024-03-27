4166dd26d9e5e2207febae792e0b4269d59063f6
Pratham Pai <59199060+pratham-pai@users.noreply.github.com>
2024-03-27 16:40:43 +0400

    Add files via upload

---
+++ 3) Secure Login System with Password Validation/Part 3/java3.java
@@ -0,0 +1,10 @@
+package com.example.signupapplication;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class loginsuccessActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_loginsuccess);
+  }
+}
